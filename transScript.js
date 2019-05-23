016967452a7042680b5b03c89abf7fbd294aaf51
StanRonzhin <thelicov@gmail.com>
2019-05-23 13:53:00 +0200

    transf

---
+++ transScript.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+
+    $('form').on('submit', (event)=>{
+
+        event.preventDefault();
+
+
+        $.getJSON("https://stanronzhin.github.io/terugmeld/data.json", function(resp) {
+            var feat = resp.features;
+            console.log(feat);
+
+            // proj4.defs([
+            //     [
+            //         'EPSG:28992',
+            //         'urn:ogc:def:crs:EPSG::28992']
+            //
+            // ]);
+
+            // Iterate over the JSON object
+            // for (var i = 0, len = feat.length; i < len; i++) {
+            //     var coordRD = feat[i].geometry.coordinates;
+            //
+            //     var coordWGS =  proj4('EPSG:28992', 'WGS84', coordRD);
+            //     tableau.log(coordWGS);
+            //
+            //     var wkt_data = new Wkt.Wkt();
+            //     wkt_data.read(coordWGS);
+            // }
+
+        });
+    });
+});
