544c49cd2e7afcc2c0c575917abff663730b441d
StanRonzhin <thelicov@gmail.com>
2019-05-23 14:12:50 +0200

    transf

--- transScript.js
+++ transScript.js
@@ -10,14 +10,7 @@ $(document).ready(function() {
             var feat = resp.features;
             console.log(feat);
 
-            // proj4.defs([
-            //     [
-            //         'EPSG:28992',
-            //         'urn:ogc:def:crs:EPSG::28992']
-            //
-            // ]);
-
-            proj4.defs('urn:x-ogc:def:crs:EPSG:28992', proj4.defs('EPSG:28992'));
+        proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
 
             for (var i = 0, len = feat.length; i < len; i++) {
                 var coordRD = feat[i].geometry.coordinates;
