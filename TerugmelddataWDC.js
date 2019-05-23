77fc92568041a77beddec3d52df0177ca2b66a8a
StanRonzhin <thelicov@gmail.com>
2019-05-23 16:44:13 +0200

    transf

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -31,32 +31,25 @@
 	myConnector.getData = function(table, doneCallback) {
 		$.getJSON("https://stanronzhin.github.io/terugmeld/data.json", function(resp) {
 			var feat = resp.features;
-				tableData = [];
-            tableau.log(feat);
+			tableData = [];
+			proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
 
-            proj4.defs([
-                [
-                    'EPSG:28992',
-                    'urn:ogc:def:crs:EPSG::28992']
-
-            ]);
-
-			// Iterate over the JSON object
+            // Iterate over the JSON object
 			for (var i = 0, len = feat.length; i < len; i++) {
+
                 var coordRD = feat[i].geometry.coordinates;
-
-               var coordWGS =  proj4('EPSG:28992', 'WGS84', coordRD);
-                tableau.log(coordWGS);
-
-               var wkt_data = new Wkt.Wkt();
-                wkt_data.read(coordWGS);
+                var coordWGS =  proj4('EPSG:28992', 'WGS84', coordRD);
+                var geoJson = '{"type":"Point","coordinates":' + JSON.stringify(coordWGS) + '}';
+                var wkt_data = new Wkt.Wkt();
+                wkt_data.read(geoJson);
+                console.log(wkt_data);
 
 
 				tableData.push({
 					"basisregistratie": feat[i].properties.basisregistratie,
 					"bronhoudernaam": feat[i].properties.bronhoudernaam,
 					"status": feat[i].properties.status,
-                    // "location": wkt_data.toJson()
+                    "location": wkt_data.toJson()
 				});
 			}
             tableau.log(tableData);
